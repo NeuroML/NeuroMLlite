bf16a54a1c40a01833ec41fb1d80220a34683811
Padraig Gleeson <p.gleeson@gmail.com>
2019-06-12 16:35:04 +0100

    More tweaks

--- examples/clean.sh
+++ examples/clean.sh
@@ -1,3 +1,3 @@
 mkdir ~/temp/dump
-mv -v report*txt *.pdf *.dat *.spikes *.gdf *.hoc *.mod  *~ *.log *nrn.py NET_*nml *brian.py *brian2.py *nest.py *pynn.py *definition.py  *netpyne.py  *moose.py  *.nestml *.png *.ini *.svg *.pov *.gv  ~/temp/dump
+mv -v report*txt *.pdf *.dat *.spikes *.gdf *.hoc *.mod  *~ *.log *nrn.py NET_*nml *brian.py *brian2.py *nest.py *pynn.py *definition.py  *netpyne.py  *moose.py  *.nestml *.png *.ini *.svg *.pov *.gv *cell.json *synapse.json *inputs.json *main.json tmp* *csv ~/temp/dump
 rm -rf x86_64 i386 umac *.pyc *~ .*~
